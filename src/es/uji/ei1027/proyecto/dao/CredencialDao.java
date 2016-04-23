092dc111387573a223464a7dd9eb917b1dbf2b6a
SPACEDAC7 <caPeLo39@gmail.com>
2016-04-23 02:52:43 +0200

    CredencialDao actualizada
    He puesto un par de comentarios

--- src/es/uji/ei1027/proyecto/dao/CredencialDao.java
+++ src/es/uji/ei1027/proyecto/dao/CredencialDao.java
@@ -1,226 +1,59 @@
 package es.uji.ei1027.proyecto.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.logging.Logger;
+import java.sql.ResultSet;
 
-import conection.ConnectionManager;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
 import es.uji.ei1027.proyecto.domain.Credencial;
 
 public class CredencialDao {
-	private final static Logger Log = Logger.getLogger(DireccionDao.class.getName()); 
-	private Connection conn = null;
+	
+	private JdbcTemplate jdbcTemplate;
+	
+	@Autowired
+	public void setDataSource(DataSource dataSource){
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+	
+	private static final class CredencialMapper implements RowMapper<Credencial>{
+		public Credencial mapRow(ResultSet rs, int rowNum) throws SQLException{
+			Credencial credencial = new Credencial();
+			credencial.setId_credencial(rs.getInt("id_credencial"));
+			credencial.setNick_usuario(rs.getString("nick_usuario"));
+			credencial.setPassword(rs.getString("password"));
+			credencial.setRol(rs.getString("rol"));
+			return credencial;
+		}
+	
+	}
+	//No se si esto es util o no y no se hasta que punto debemos enseñar la contraseña SEGURIDAD
+	public List<Credencial> getCredenciales(){
+		return this.jdbcTemplate.query("select id_credencial,nick_usuario, password,rol from credencial", new CredencialMapper());
+	}
 	
 	public Credencial getCredencial(int id_credencial) {
-		Connection conn = null;
-		try {
-			conn = ConnectionManager.getConnection();
-		}
-		catch (ClassNotFoundException e) {
-			Log.severe("Driver JDBC no trobat");
-			e.printStackTrace();
-			return null;
-		}
-		catch (SQLException e) {
-			Log.severe("Error creant connexiï¿½ JDBC");
-			e.printStackTrace();
-			return null;
-		}
-		PreparedStatement stmt = null;
-		ResultSet rs = null;
-		Credencial credencial = null;
-		try {
-			String sql = "SELECT id_credencial, nick_usuario, password, rol " + 
-						"FROM credencial WHERE id_credencial = ?";
-			stmt = conn.prepareStatement(sql);
-			stmt.setInt(1, id_credencial);
-			rs = stmt.executeQuery();
-			if ( rs != null){
-				while(rs.next()){
-					credencial.setId_credencial(rs.getInt(1));
-					credencial.setNick_usuario(rs.getString(2));
-					credencial.setPassword(rs.getString(3));
-					credencial.setRol(rs.getString(4));
-				}
-			}
-		} 
-		catch (SQLException e) {
-			Log.severe("Error executant PreparedStatement");
-			e.printStackTrace();
-			return null;
-		}
-		finally {
-			if (rs != null) {
-				try {
-					rs.close();
-				}
-				catch (SQLException e) {
-					Log.warning("Error tancant ResultSet");
-					e.printStackTrace();
-				}
-			}
-			if (stmt != null) {
-				try {
-					stmt.close();
-				} catch (SQLException e) {
-					Log.warning("Error tancant PreparedStatement");
-					e.printStackTrace();
-				}
-			}
-			try {
-				conn.close();
-			} catch (SQLException e) {
-				Log.warning("Error tancant connexiï¿½ JDBC");				
-				e.printStackTrace();
-			}
-		}
-		return credencial;
+		return this.jdbcTemplate.queryForObject("SELECT id_credencial, nick_usuario, password, rol FROM credencial WHERE id_credencial = ?",  new Object[] {id_credencial}, new CredencialMapper()); 
 	}
 	
 	
 	public void addCredencial(Credencial credencial) {
-		Connection conn = null;
-		try {
-			conn = ConnectionManager.getConnection();
-		}
-		catch (ClassNotFoundException e) {
-			Log.severe("Driver JDBC no trobat");
-			e.printStackTrace();
-			return;
-		}
-		catch (SQLException e) {
-			Log.severe("Error creant connexiï¿½ JDBC");
-			e.printStackTrace();
-			return;
-		}
-		PreparedStatement stmt = null;
-		try {
-			 stmt = conn.prepareStatement(
-					"insert into credencial(id_credencial, nick_usuario, password , rol) "
-					+ "values(?, ?, ?, ?)");
-			stmt.setInt(1, credencial.getId_credencial());
-			stmt.setString(2, credencial.getNick_usuario());
-			stmt.setString(3, credencial.getPassword());
-			stmt.setString(4, credencial.getRol());
-			stmt.execute();	
-		}
-		catch (SQLException e) {
-			Log.severe("Error executant PreparedStatement");
-			e.printStackTrace();
-		}
-		finally {
-			if (stmt != null) {
-				try {
-					stmt.close();
-				} catch (SQLException e) {
-					Log.warning("Error tancant PreparedStatement");
-					e.printStackTrace();
-				}
-			}
-			try {
-				conn.close();
-			} catch (SQLException e) {
-				Log.warning("Error tancant connexiï¿½ JDBC");				
-				e.printStackTrace();
-			}
-		}
+		this.jdbcTemplate.update("insert into credencial(id_credencial, nick_usuario, password , rol) values(?, ?, ?, ?)", credencial.getId_credencial(), credencial.getNick_usuario(),credencial.getPassword(),credencial.getRol());
 	}
 	
 	public void updateCredencial(Credencial credencial) {
-		Connection conn = null;
-		try {
-			conn = ConnectionManager.getConnection();
-		}
-		catch (ClassNotFoundException e) {
-			Log.severe("Driver JDBC no trobat");
-			e.printStackTrace();
-			return;
-		}
-		catch (SQLException e) {
-			Log.severe("Error creant connexiï¿½ JDBC");
-			e.printStackTrace();
-			return;
-		}
-		PreparedStatement stmt = null;
-		try {
-			stmt = conn.prepareStatement(
-					"update credencial "
-					+ "set nick_usuario = ?, "
-					+ "password = ?, "
-					+ "rol = ? "
-					+ "where id_credencial = ?");
-			stmt.setString(1, credencial.getNick_usuario());
-			stmt.setString(2, credencial.getPassword());
-			stmt.setString(3, credencial.getRol());
-			stmt.setInt(4, credencial.getId_credencial());
-			stmt.execute();	
-		}
-		catch (SQLException e) {
-			Log.severe("Error executant PreparedStatement");
-			e.printStackTrace();
-		}
-		finally {
-			if (stmt != null) {
-				try {
-					stmt.close();
-				} catch (SQLException e) {
-					Log.warning("Error tancant PreparedStatement");
-					e.printStackTrace();
-				}
-			}
-			try {
-				conn.close();
-			} catch (SQLException e) {
-				Log.warning("Error tancant connexiï¿½ JDBC");				
-				e.printStackTrace();
-			}
-		}
+		this.jdbcTemplate.update("update Credencial set nick_usuario = ?, password = ?, rol = ? where id_credencial = ?", credencial.getNick_usuario(),credencial.getPassword(),credencial.getRol(),credencial.getId_credencial());
 	}
-
+	//No se si deberiamos borrar con un argumento credencial o con un argumento int id_credencial
 	public void deleteCredencial(Credencial credencial) {
-		Connection conn = null;
-		try {
-			conn = ConnectionManager.getConnection();
-		}
-		catch (ClassNotFoundException e) {
-			Log.severe("Driver JDBC no trobat");
-			e.printStackTrace();
-			return;
-		}
-		catch (SQLException e) {
-			Log.severe("Error creant connexiï¿½ JDBC");
-			e.printStackTrace();
-			return;
-		}
-		PreparedStatement stmt = null;
-		try {
-			stmt = conn.prepareStatement(
-					"DELETE FROM credencial WHERE id_credencial = ?");
-			stmt.setInt(1, credencial.getId_credencial());
-			stmt.execute();	
-		}
-		catch (SQLException e) {
-			Log.severe("Error executant PreparedStatement");
-			e.printStackTrace();
-		}
-		finally {
-			if (stmt != null) {
-				try {
-					stmt.close();
-				} catch (SQLException e) {
-					Log.warning("Error tancant PreparedStatement");
-					e.printStackTrace();
-				}
-			}
-			try {
-				conn.close();
-			} catch (SQLException e) {
-				Log.warning("Error tancant connexiï¿½ JDBC");				
-				e.printStackTrace();
-			}
-		}
+		this.jdbcTemplate.update("DELETE FROM Credencial WHERE id_credencial = ?", credencial.getId_credencial());
 	}
 	
 }
