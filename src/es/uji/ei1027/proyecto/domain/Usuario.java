dd9e2f46837c6c07a5497fbfcf28b5ed401db156
SPACEDAC7 <caPeLo39@gmail.com>
2016-05-06 23:45:30 +0200

    Comentario es usuario

--- src/es/uji/ei1027/proyecto/domain/Usuario.java
+++ src/es/uji/ei1027/proyecto/domain/Usuario.java
@@ -16,7 +16,7 @@ public class Usuario {
 	private String telefono;
 	private boolean estado_usuario;
 	//Tengo que poner en la base de datos que sea un varchar(20)
-	//OK
+	//Esto es una prueba a ver que pasa
 	
 	private String fechaRegistro;
 	
